7c4034135cf73e863df66a4404fa871db673f126
0dj0bz <rob@abbottics.ai>
2020-08-25 17:37:58 -0400

    created shell script to traverse artifact snippets and produce catalog.

---
+++ create-art-catalog.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+rootdn1='/mnt/f5c6f0d4-e553-4895-a955-e0f62ee703f4'
+snippet_subdir_dst='/tuh_eeg_art_snippets'
+snippet_dst=$rootdn1$snippet_subdir_dst
+
+
+for tld1 in `ls $snippet_dst`; do
+	# echo $tld1
+	for tld2 in `ls $snippet_dst/$tld1`; do
+		# echo $tld2
+		for tld3 in `ls $snippet_dst/$tld1/$tld2`; do
+			# echo $tld3
+			for tld4 in `(cd $snippet_dst/$tld1/$tld2/$tld3/ && ls -1 *.art)`
+			do 
+				fname=$snippet_dst/$tld1/$tld2/$tld3/$tld4
+        `build-catalog $fname >> $snippet_dst/snippet-catalog.tsv`
+			done
+		done
+	done
+done
+
